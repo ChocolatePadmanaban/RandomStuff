8a8cd5e33a2a8c54092357c294cd613a7e99aaf0
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-04-26 02:45:26 +0530

    Please god help me

--- ML/Home Work 4/main.py
+++ ML/Home Work 4/main.py
@@ -6,4 +6,17 @@ import em
 
 X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+def Lowest_cost():
+    K= [1,2,3,4]
+    for k in K:
+        minimumcost= np.inf
+        for seedvalue in range(5):
+            mixture, post = common.init(X,k,seedvalue)
+            mixture, post, cost = naive_em.run(X,mixture,post)
+            print("seed values with k",k,seedvalue)
+            minimumcost = min(cost,minimumcost)
+        print("Cost of k:",k,"is cost:",minimumcost)
+        common.plot(X,mixture,post,"Cost of k:"+str(k)+"is cost:"+str(minimumcost))
+
+if __name__ =="__main__":
+    Lowest_cost()
--- ML/Home Work 4/naive_em.py
+++ ML/Home Work 4/naive_em.py
@@ -17,7 +17,20 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    n, d = X.shape
+    K, _ = mixture.mu.shape
+    postnorm = np.zeros((n, K))    
+    
+
+    for i, row in enumerate(X):
+        for j in range(K):            
+            postnorm[i,j] = np.exp((np.linalg.norm(row-mixture.mu[j]))**2/(-2*mixture.var[j]))/(2*np.pi*mixture.var[j])**(d/2)
+    postcol= postnorm.sum(axis=1)
+    post = postnorm/postcol [:,None]
+    postlog = np.sum(np.log(np.sum(post*postnorm,axis=1)))
+
+    return post , postlog
+
 
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
@@ -32,7 +45,23 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, d = X.shape
+    _, K = post.shape
+
+    n_hat = post.sum(axis=0)
+    p = n_hat / n
+
+    cost = 0
+    mu = np.zeros((K, d))
+    var = np.zeros(K)
+
+    for j in range(K):
+        mu[j, :] = post[:, j] @ X / n_hat[j]
+        sse = ((mu[j] - X)**2).sum(axis=1) @ post[:, j]
+        cost += sse
+        var[j] = sse / (d * n_hat[j])
+
+    return GaussianMixture(mu, var, p), cost
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -50,4 +79,12 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    prev_cost = None
+    cost = None
+    while (prev_cost is None or prev_cost - cost > 1e-4):
+        prev_cost = cost
+        post, _ = estep(X, mixture)
+        print(_)
+        mixture, cost = mstep(X, post)
+
+    return mixture, post, cost
