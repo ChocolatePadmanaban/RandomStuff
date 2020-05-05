2843ce1d8571e70b12e7c24a2a88481897ccd979
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-05-05 21:53:31 +0530

    work hard

--- ML/RL/problem1.py
+++ ML/RL/problem1.py
@@ -37,15 +37,22 @@ transition_matrix= np.array([[[0.5, 2/3 + 1/6],
 transition_matrix = transition_matrix 
 print(transition_matrix)
 reward = np.array([0,0,0,0,1])
-reward = np.ones((5,5,2)) * reward [None,:, None]
+reward = np.ones((5,5,2))*reward[None,:,None]
+#reward[4,4,1]= 1
 print(reward)
-V_star = np.zeros(5)
+V_star= np.zeros(5)
+
 
 def Value_iteration (transition_matrix, reward, V_star, g=.5 ):
-    V_star= np.max(np.sum(transition_matrix*(reward+ g*V_star[None,:,None]*np.ones((5,5,2))), axis=1)  , axis=1)
-    return V_star
+    V_exec = np.ones((5,5,2))*V_star[None,: , None]
+    #print("V_exec", V_exec)
+    Q_star = np.sum(transition_matrix*(reward+ g*V_exec), axis=1)
+    #print("Q_star",Q_star)
+    return np.max(Q_star, axis=1)
+
+  
 
 for i in range(100):
     V_star=Value_iteration (transition_matrix, reward, V_star )
-    print(V_star)
+    print("V_star",V_star)
 
--- ML/RL/problem4.py
+++ ML/RL/problem4.py
@@ -1,50 +1,19 @@
-#Re working on problem 2
 import numpy as np
-
-# row -> initial state , column -> destination state and extention -> actions 
-
-transition_matrix= np.array([[[0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.]],
-
-       [[0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.]],
-
-       [[0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.]],
-
-       [[0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.]],
-
-       [[0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.],
-        [0., 0.]]])
+V = np.array([0,0,0,0])
+reward_down = np.array([1,1,10,0])
+reward_up= np.array([0,1,1,10])
+gamma = 1/3
+up = np.array([[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]],dtype=float)
+down = np.array([[0,0,0,0],[1,0,0,0],[0,1,0,0],[0,0,1,0]],dtype=float)
 
 
-transition_matrix = transition_matrix 
-print(transition_matrix)
-reward = np.array([0,0,0,0,1])
 
-V_star = np.zeros(5)
 
-def Value_iteration (transition_matrix, reward, V_star, g=.5 ):
-    multiplier= reward+ g*V_star
-    V_star= np.sum( multiplier * transition_matrix, axis=1)
-    return V_star
-
-for i in range(100):
-    V_star=Value_iteration (transition_matrix, reward, V_star )
-    print(V_star)
+for i in range(0,100):
+    up_action = up@(gamma*V+reward_up)
+    print("up_action", up_action)
+    down_action = down@(gamma*V+reward_down)
+    print("down_action",down_action)
+    transition_choices = np.stack([up_action,down_action])
+    V = np.max(transition_choices, axis=0)
+    print(V)
