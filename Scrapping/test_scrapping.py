679a3f1c1e903fbbf38ac033cfa6953ada43cfd5
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-06-19 00:58:47 +0530

    web scrapping

---
+++ Scrapping/test_scrapping.py
@@ -0,0 +1,27 @@
+# test_scrapping.py
+
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get("https://www.google.com")
+
+#print status code 
+print("Stadus code",result.status_code)
+
+# for status code refer https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+#print headers
+print("Headers: ", result.headers)
+#https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+
+#source of the page
+src = result.content
+#print("\n Source : ",src)
+
+
+#create Beautiful soup object from src
+soup = BeautifulSoup(src, 'lxml')
+
+#find all the links
+links= soup.find_all('a')
+print(links, '\n')
