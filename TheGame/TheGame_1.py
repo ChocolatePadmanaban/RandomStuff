980bd50e466a7284e50e1157fd291d4b03fee0b7
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-12-26 23:32:27 +0530

    update pygame

--- TheGame/TheGame_1.py
+++ TheGame/TheGame_1.py
@@ -18,5 +18,8 @@ while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    
+    pygame.draw.rect(win,(255,0,0), (x,y,width,height))
+    pygame.display.update()
 
 pygame.quit()
