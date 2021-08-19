737bf10d31c498f583b1f2ad61e07e619a5466f8
Pradeep Padmanaban C <38208957+ChocolatePadmanaban@users.noreply.github.com>
2021-08-19 15:27:06 +0530

    Reverting mistakes

--- loadtest/https_server_threads.py
+++ loadtest/https_server_threads.py
@@ -1,28 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
-import threading, os , sqlite3 , datetime
-import socket
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-server_ip = s.getsockname()[0]
-s.close()
-server_port = 8080
-db_filename = 'server_log.db'
-schema_filename = 'server_log_schema.sql'
-
-db_is_new = not os.path.exists(db_filename)
-
-with sqlite3.connect(db_filename) as conn:
-    if db_is_new:
-        print('Creating schema')
-        with open(schema_filename, 'rt') as f:
-            schema = f.read()
-        conn.executescript(schema)
-
-    else:
-        print('Database exists, assume schema does, too.')
+import threading
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -34,13 +12,14 @@ class Handler(BaseHTTPRequestHandler):
         message = threading.currentThread().getName()
         self.wfile.write(message.encode('utf-8'))
         self.wfile.write(b'\n')
-        with sqlite3.connect(db_filename) as conn:
-            conn.execute("INSERT INTO log_data VALUES (?,?,?,?,?)",(int(datetime.datetime.strptime(self.date_time_string(),'%a, %d %b %Y %H:%M:%S %Z').timestamp()),self.client_address[0],self.client_address[1],server_ip,server_port))
+        with open('server.log','a') as log_file:
+            log_file.write(str(self.client_address)+' '+self.log_date_time_string()+' '+self.request_version+'\n')
+
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
 
 if __name__ == '__main__':
-    server = ThreadedHTTPServer((server_ip, server_port), Handler)
-    print('Starting server, use <Ctrl-C> to stop', server_ip)
+    server = ThreadedHTTPServer(('localhost', 8080), Handler)
+    print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
