fe468d160995ae7ebf7e48498d767a452c8836b2
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2021-08-29 14:58:34 +0530

    golang http test

---
+++ loadtest/golang_loadtest/Example1_http_server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	// Custom http server
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        http.HandlerFunc(myHandler),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Server failed: ", err.Error())
+	}
+}
